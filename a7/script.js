93d1fa67a8cbd3bb7b8fd77b1ba3c8eb2e5276cd
Charan0413 <charanyadav417@gmail.com>
2025-05-14 19:02:54 +0530

    updated

---
+++ a7/script.js
@@ -0,0 +1,26 @@
+let resultHTML = ''; 
+
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+const getresult = () => {
+  const n1 = parseFloat(document.getElementById('n1').value);
+  const n2 = parseFloat(document.getElementById('n2').value);
+
+   resultHTML = `
+      Addition: ${addition(n1, n2)}<br>
+      Subtraction: ${subtraction(n1, n2)}<br>
+      Multiplication: ${multiplication(n1, n2)}<br>
+      Division: ${division(n1, n2)}
+    `;
+  
+
+  document.getElementById('output').innerHTML = '';
+};
+
+const dispResult = () => {
+  document.getElementById('output').innerHTML = resultHTML;
+};
+
